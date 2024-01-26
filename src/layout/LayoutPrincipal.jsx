5d1fba6f403551c9ad915189020f6fda81bef458
Camilo Velasquez <camilox168@outlook.com>
2024-01-26 15:12:43 -0500

    fixing header

--- src/layout/LayoutPrincipal.jsx
+++ src/layout/LayoutPrincipal.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 
-import Header from '../components/header';
+import Header from '../components/Header';
 import Footer from '../components/Footer.jsx';
 
 const LayoutPrincipal = () => {
